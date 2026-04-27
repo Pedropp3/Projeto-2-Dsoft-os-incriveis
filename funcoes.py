094d5766e5052044181472e74ea1086e6931f1ba
Pedro Penteado <pedropp3@al.insper.edu.br>
2026-04-26 22:07:23 -0300

    tentativa 2 ex 1

--- funcoes.py
+++ funcoes.py
@@ -10,6 +10,6 @@ def rolar_dados (Ndados):
     return resultado
 
 def guardar_dado (dados_rolados, dados_no_estoque, dado_para_guardar):
-    dados_no_estoque.append(dados_rolados[dado_para_guardar-1])
-    dados_rolados.pop(dado_para_guardar-1)
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+    dados_rolados.pop(dado_para_guardar)
     return [dados_rolados,dados_no_estoque]
