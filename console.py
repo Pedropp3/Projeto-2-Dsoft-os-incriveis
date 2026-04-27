9d397b485163e21a79607adee4a2fef8d6b714a3
Pedro Penteado <pedropp3@al.insper.edu.br>
2026-04-26 22:01:08 -0300

    tentativa1 exercicio 2

--- console.py
+++ console.py
@@ -1,1 +1,6 @@
 from funcoes import *
+dados_rolados = [1, 3, 2]
+dados_no_estoque = [1, 2]
+dado_para_guardar = 1
+
+print(guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar))
--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,7 @@ def rolar_dados (Ndados):
         i = i+1
     return resultado
 
+def guardar_dado (dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_no_estoque.append(dados_rolados[dado_para_guardar-1])
+    dados_rolados.pop(dado_para_guardar-1)
+    return [dados_rolados,dados_no_estoque]
