72956bbab9f9bb63cce83c31c88a6ecaa52f6e70
Pedro Penteado <pedropp3@al.insper.edu.br>
2026-04-27 01:15:24 -0300

    exercicio 3

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,8 @@ def guardar_dado (dados_rolados, dados_no_estoque, dado_para_guardar):
     dados_no_estoque.append(dados_rolados[dado_para_guardar])
     dados_rolados.pop(dado_para_guardar)
     return [dados_rolados,dados_no_estoque]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    dados_rolados.append(dados_no_estoque[dado_para_remover])
+    dados_no_estoque.pop(dado_para_remover)
+    return [dados_rolados, dados_no_estoque]
