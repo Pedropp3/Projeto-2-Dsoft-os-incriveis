47fb94c789abd34d3bf2e16b4413824114e1bef8
Pedro Penteado <pedropp3@al.insper.edu.br>
2026-04-27 01:43:59 -0300

    ex 7

--- console.py
+++ console.py
@@ -1,3 +1,3 @@
 from funcoes import *
 
-print(calcula_pontos_sequencia_baixa([5, 5, 5, 2, 2]))
+print(calcula_pontos_sequencia_alta([2, 3, 4, 6, 2]))
--- funcoes.py
+++ funcoes.py
@@ -49,3 +49,16 @@ def calcula_pontos_sequencia_baixa (faces):
         return 15
     else:
         return 0
+    
+def calcula_pontos_sequencia_alta (faces):
+    sim = 0
+    for f in faces:
+        if f + 1 in faces:
+            if f+2 in faces:
+                if f+3 in faces:
+                    if f+4 in faces:
+                        sim = 1
+    if sim == 1:
+        return 30
+    else:
+        return 0
